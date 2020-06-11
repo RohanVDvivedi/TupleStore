bf20963aada6f338a59a33f97ec4bcc050d05a11
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-11 15:28:21 +0530

    layout helper functions debugged

--- inc/layout_helper.h
+++ inc/layout_helper.h
@@ -12,7 +12,7 @@ inline void* get_page_content(page_context* pg_cntxt)
 	return page + sizeof(page_hdr);
 }
 
-inline uint16_t get_total_page_content_size(page_context* pg_cntxt)
+inline uint16_t get_page_content_size(page_context* pg_cntxt)
 {
 	return pg_cntxt->dam->page_size_in_bytes - sizeof(page_hdr);
 }
