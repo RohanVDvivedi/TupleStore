b92efdb20b4007daecaba7bd0a4d29f08c85727d
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-11 15:28:21 +0530

    layout helper functions debugged

--- inc/slotted_page_helper.h
+++ inc/slotted_page_helper.h
@@ -1,40 +1,46 @@
 #ifndef SLOTTED_PAGE_HELPER
 #define SLOTTED_PAGE_HELPER
 
-uint16_t get_number_of_tuples_stored(page_context* pg_cntxt)
+inline uint16_t get_number_of_tuples_stored(page_context* pg_cntxt)
 {
 	if(pg_cntxt->tuples_stored != 0)
 	{
 		return pg_cntxt->tuples_stored;
 	}
 
-	void* page_content_area = get_page_content(pg_cntxt);
+	void* page_content = get_page_content(pg_cntxt);
 
 	// the first 16 bits represent the number of tuples currently stored in the page
-	// hence the + 2
 
-	pg_cntxt->tuples_stored = *((uint16_t*)(page_content_area));
+	pg_cntxt->tuples_stored = *((uint16_t*)(page_content));
 	return pg_cntxt->tuples_stored;
 }
 
-uint16_t* get_tuple_offsets(page_context* pg_cntxt)
+inline uint16_t* get_tuple_offsets(page_context* pg_cntxt)
 {
-	void* page_content_area = get_page_content(page_context* pg_cntxt);
+	void* page_content = get_page_content(pg_cntxt);
 
 	// the first 16 bits represent the number of tuples currently stored in the page
 	// hence the + 2
+	// and then follows the array of 16 bit integers stating the offsets within page for each tuple that is stored
 
-	// and them follows the array of 16 bit integers stating the offsets within page for each tuple that is stored
-
-	return (uint16_t*)(page_content_area + 2);
+	return (uint16_t*)(page_content + 2);
 }
 
 // you must provide a tuple_no < tuples_stored for the page
 // tuple offset being 0 mean, that there does not exist a tuple at that location
-int is_tuple_deleted(page_context* pg_cntxt, uint16_t tuple_no)
+inline int is_tuple_deleted(page_context* pg_cntxt, uint16_t tuple_no)
 {
 	uint16_t* tuple_offsets = get_tuple_offsets(pg_cntxt);
-	return tuple_offsets[tuples_no] == 0;
+	return tuple_offsets[tuple_no] == 0;
+}
+
+// you must provide a tuple_no < tuples_stored for the page
+// tuple offset being 0 mean, that there does not exist a tuple at that location
+inline void mark_tuple_deleted(page_context* pg_cntxt, uint16_t tuple_no)
+{
+	uint16_t* tuple_offsets = get_tuple_offsets(pg_cntxt);
+	tuple_offsets[tuple_no] = 0;
 }
 
 #endif
--- inc/tuple_array_helper.h
+++ inc/tuple_array_helper.h
@@ -2,55 +2,67 @@
 #define TUPLE_ARRAY_HELPER
 
 // maximum number of tuples that you can store in a tuple array storage layout
-uint16_t tuple_storage_limit(page_context* pg_cntxt)
+inline uint16_t tuple_storage_limit(page_context* pg_cntxt)
 {
 	if(pg_cntxt->tuple_storage_limit != 0)
 	{
 		return pg_cntxt->tuple_storage_limit;
 	}
 
-	uint16_t total_page_content_size = get_total_page_content_size(pg_cntxt);
+	uint16_t page_content_size = get_page_content_size(pg_cntxt);
 
 	/*
-	**	total bits for page content = 8 * total_page_content_size
-	**	bits required to store a tuple = 8 * page_hdr->tuple_definition->size + 1
-	**	extra 1 lets us store if the tuple is deleted or not
+	**	total bits for page content = 8 * page_content_size
+	**	bits required to store a tuple = (8 * page_hdr->tuple_definition->size) + 1
+	**	extra 1 lets us store if the tuple exists at that index or not
 	**  tuples_limit = total bits for page content / bits required to store a tuple
 	*/
 
-	pg_cntxt->tuple_storage_limit = (8 * total_page_content_size) / (8 * page_hdr->tuple_definition->size + 1);
+	pg_cntxt->tuple_storage_limit = (8 * page_content_size) / (8 * pg_cntxt->tuple_definition->size + 1);
 
 	return pg_cntxt->tuple_storage_limit;
 }
 
-// array of bytes, where each bit is used to mark if a tuple is deleted or not
-uint8_t* tuple_deleted_bits(page_context* pg_cntxt)
+// array of bytes, where each bit is used to mark if a tuple exists at that position or not
+inline uint8_t* tuple_exists_bits(page_context* pg_cntxt)
 {
 	void* page_content = get_page_content(pg_cntxt);
 	// tuple deleted bits are the first thing stored in the tuple array layout in the 
 	return (uint8_t*)(page_content);
 }
 
-// size of array of bytes, where each bit is used to mark if a tuple is deleted or not
-uint16_t tuple_deleted_bits_size_in_bytes(page_context* pg_cntxt)
+// size of array of bytes, where each bit is used to mark if a tuple exists at that position or not
+inline uint16_t tuple_exists_bits_size_in_bytes(page_context* pg_cntxt)
 {
 	return (pg_cntxt->tuple_storage_limit / 8) + (((pg_cntxt->tuple_storage_limit % 8) > 0) ? 1 : 0);
 }
 
-void* get_tuples(page_context* pg_cntxt)
+inline void* get_tuples(page_context* pg_cntxt)
 {
 	void* page_content = get_page_content(pg_cntxt);
 
 	// tuples are stored after the deleted bits array 
 
-	return page_content + tuple_deleted_bits_size_in_bytes(pg_cntxt);
+	return page_content + tuple_exists_bits_size_in_bytes(pg_cntxt);
 }
 
-// you must provide a tuple_no < tuple_storage_limit for the page
-int is_tuple_deleted(page_context* pg_cntxt, uint16_t tuple_no)
+// checks if tuple exists at a particular tuple_no in the page
+inline int does_tuple_exist(page_context* pg_cntxt, uint16_t tuple_no)
 {
-	uint8_t* deleted_bits = tuple_deleted_bits(pg_cntxt);
-	return deleted_bits[tuples_no / 8] & (1 << (tuples_no % 8));
+	uint8_t* exist_bits = tuple_exists_bits(pg_cntxt);
+	return (exist_bits[tuple_no/8]&(1<<(tuple_no%8)));
+}
+
+inline void unset_tuple_exist(page_context* pg_cntxt, uint16_t tuple_no)
+{
+	uint8_t* exist_bits = tuple_exists_bits(pg_cntxt);
+	exist_bits[tuple_no/8] &= (~(1<<(tuple_no%8)));
+}
+
+inline void set_tuple_exist(page_context* pg_cntxt, uint16_t tuple_no)
+{
+	uint8_t* exist_bits = tuple_exists_bits(pg_cntxt);
+	exist_bits[tuple_no/8] |= (1<<(tuple_no%8));
 }
 
 #endif
