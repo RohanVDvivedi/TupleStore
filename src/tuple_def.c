d7276d97e51363c9cc6f9c7b12edfa91c5ee828a
Rohan Dvivedi <rohandvivedi@gmail.com>
2021-03-02 22:22:43 +0530

    basic tuple and tuple definition started

--- src/tuple_def.c
+++ src/tuple_def.c
@@ -1,81 +1,104 @@
 #include<tuple_def.h>
 
-char type_as_string[][16] = {
-								"CHAR_STRING",
-								"SIGNED_INT",
-								"UNSIGNED_INT",
-								"FLOATING_NUM"
+char type_as_string[][6] = {
+								"UINT",
+								"INT",
+								"FLOAT",
+								"BLOB"
 							};
 
-int is_size_allowed(type element_type, byte_size size)
+static int is_size_allowed(type element_type, uint64_t size)
 {
 	switch(element_type)
 	{
-		case CHAR_STRING :
-			return (size <= CHAR_STRING_max_length);
-		case SIGNED_INT :
-		case UNSIGNED_INT :
+		case UINT :
+		case INT :
 			return (size == 1) || (size == 2) || (size == 4) || (size == 8);
-		case FLOATING_NUM :
+		case FLOAT :
 			return (size == 4) || (size == 8);
+		case STRING :
+		case BLOB :	// STRING and BLOB can be of size VARIABLE_SIZED
+			return 1;
 		default :
 			return 0;
 	}
 }
 
-static int init_col_def(element_def* element_d, type type, byte_size size)
+static int init_element_def(element_def* element_d, type type, uint64_t size)
 {
-	if(is_size_allowed(type, size))
-	{
-		element_d->type = type;
-		element_d->size = size;
-		return 1;
-	}
-	return 0;
+	if(!is_size_allowed(type, size))
+		return 0;
+
+	element_d->type = type;
+	element_d->size = size;
+	element_d->byte_offset = 0;
+	return 1;	
 }
 
 void init_tuple_def(tuple_def* tuple_d)
 {
-	tuple_d->key_size = 0;
-	tuple_d->key_element_count = 0;
 	tuple_d->size = 0;
 	tuple_d->element_count = 0;
 }
 
-int insert_element_def(tuple_def* tuple_d, type element_type, uint16_t element_size)
+int insert_element_def(tuple_def* tuple_d, type element_type, uint64_t element_size)
 {
-	element_def* new_element_def = &(tuple_d->element_defs[tuple_d->element_count]);
-	if(init_col_def(new_element_def, element_type, element_size))
-	{
-		new_element_def->offset = tuple_d->size;
+	// for a variable sized element
+	// the size must be given by a preceding element of UINT data type
+	if(element_size == VARIABLE_SIZED && tuple_d->element_defs[tuple_d->element_count - 1].type != UINT)
+		return 0;
+
+	// if an element is not of appropriate size it can not be initialized
+	element_def* new_element_def = tuple_d->element_defs + tuple_d->element_count;
+	if(!init_element_def(new_element_def, element_type, element_size))
+		return 0;
+
+	new_element_def->byte_offset = tuple_d->size;
+	
+	// update the tuple size
+	if(element_size == VARIABLE_SIZED)
+		tuple_d->size = 0;	// if a VARIABLE_SIZED element is encountered, then restart counting
+	else
 		tuple_d->size += new_element_def->size;
-		tuple_d->element_count += 1;
-		return 1;
-	}
-	return 0;
+
+	// increment element count in the tuple
+	tuple_d->element_count += 1;
+	return 1;
 }
 
-void tuple_mark_key_complete(tuple_def* tuple_d)
+void finalize_tuple_def(tuple_def* tuple_d)
 {
-	for(int i = tuple_d->key_element_count; i < tuple_d->element_count; i++)
+	for(int i = 0; i < tuple_d->element_count; i++)
 	{
-		tuple_d->key_size = tuple_d->size;
-		tuple_d->key_element_count = tuple_d->element_count;
+		if(tuple_d->element_defs[i].size == VARIABLE_SIZED)
+		{
+			tuple_d->size = VARIABLE_SIZED;
+			return;
+		}
 	}
+}
+
+static void print_element_def(element_def* element_d)
+{
+	printf("\t\t\t type : %s\n", type_as_string[element_d->type]);
+	if(element_d->size == VARIABLE_SIZED)
+		printf("\t\t\t size : VARIABLE_SIZED (0)\n");
+	else
+		printf("\t\t\t size : %lu\n", element_d->size);
+	printf("\t\t\t byte_offset : %lu\n", element_d->byte_offset);
 }
 
 void print_tuple_def(tuple_def* tuple_d)
 {
-	printf("Tuple definition       : \n");
-	printf("\t tuple_size in bytes : %u\n", tuple_d->size);
-	printf("\t elements            : %u\n", tuple_d->element_count);
-	printf("\t key_size in bytes   : %u\n", tuple_d->key_size);
-	printf("\t key_elements        : %u\n", tuple_d->key_element_count);
-	for(u2 i = 0; i < tuple_d->element_count; i++)
+	printf("Tuple definition : \n");
+	if(tuple_d->size == VARIABLE_SIZED)
+		printf("\t tuple_size : VARIABLE_SIZED (0)\n");
+	else
+		printf("\t tuple_size : %lu\n", tuple_d->size);
+	printf("\t elements : (%lu)\n", tuple_d->element_count);
+	for(uint64_t i = 0; i < tuple_d->element_count; i++)
 	{
-		printf("\t\tColumn : %u\n", i);
-		printf("\t\t\ttype              : %s\n", type_as_string[tuple_d->element_defs[i].type]);
-		printf("\t\t\tsize  (bytes)     : %u\n", tuple_d->element_defs[i].size);
-		printf("\t\t\toffset in tuple   : %u\n", tuple_d->element_defs[i].offset);
+		printf("\t\t Column : %lu\n", i);
+		print_element_def((tuple_d->element_defs) + i);
 	}
 }
