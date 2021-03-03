f8a780a43f6d599c65e36b43564efad8b7ccb2a1
Rohan Dvivedi <rohandvivedi@gmail.com>
2021-03-03 18:37:04 +0530

    added all method definitions required for page_layout.h

--- inc/page_layout.h
+++ inc/page_layout.h
@@ -9,31 +9,51 @@
 
 
 
-// returns true if the given page has enough space to accomodate the given tuple
-int can_page_accomodate_this_tuple();
-
 // to insert a tuple at the given index in the page
-int insert_tuple_in_page();
+int insert_tuple(void* page, uint64_t page_size, const tuple_def* tpl_d, const void* external_tuple);
 
 // to remove a tuple at the given index in the page
-int remove_tuple_from_page();
+int delete_tuple(void* page, uint64_t page_size, const tuple_def* tpl_d, uint64_t index);
+
+// to check if a tuple at the given index in the page has been deleted
+int is_deleted_tuple(const void* page, uint64_t page_size, const tuple_def* tpl_d, uint64_t index);
+
+
 
 // returns the index of the last tuple in the page
-uint64_t get_last_tuple_index();
+uint64_t get_last_tuple_index(const void* page, uint64_t page_size, const tuple_def* tpl_d);
 
 
 
-// ALL SEEK FUNCTIONS FAIL WITH NULL, WHEN THE INDEX OF THE TUPLE IS OUT_OF_BOUNDS 
-// tuple is out of bounds, if (index < 0 || index > get_last_tuple_index())
 
-// returns pointer to the first tuple in the page
-void* seek_to_first_tuple();
-
-// returns pointer to the next tuple in the page
-// i.e. from nth tuple to n+1 th tuple in the page
-void* seek_to_next_tuple();
+// SEEK FUNCTIONS FAIL WITH NULL, WHEN THE INDEX OF THE TUPLE IS OUT_OF_BOUNDS 
+// tuple is out of bounds, if (index > get_last_tuple_index())
 
 // returns pointer to nth tuple in the page
-void* seek_to_nth_tuple();
+void* seek_to_nth_tuple(const void* page, uint64_t page_size, const tuple_def* tpl_d, uint64_t index);
+
+
+
+
+// returns true if the given page has enough space to accomodate the given tuple
+int can_accomodate_tuple(const void* page, uint64_t page_size, const tuple_def* tpl_d, const void* external_tuple);
+
+// returns total free space inside a given page
+// this is the space that will be freed if the page was to be compacted thoroughly
+uint64_t get_free_space(const void* page, uint64_t page_size, const tuple_def* tpl_d);
+
+
+
+
+// for a slotted page, compaction of the page is necessary to increase the usable area
+// it involves
+//    * reducing the extra area given to the VARIABLE_SIZED elements in the tuple
+//    * removing tomb stones for deleted records and their records space in both VARIABLE_SIZED and fixed sized tuples
+int compact_page(const void* page, uint64_t page_size, const tuple_def* tpl_d);
+
+
+
+// prints all tuples in the page including the deleted ones with their tomb stones
+void print_all_tuples_in_page(const void* page, uint64_t page_size, const tuple_def* tpl_d);
 
 #endif
