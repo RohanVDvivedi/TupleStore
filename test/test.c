95cc9bad135845bd4af3fd7ecdc3f6253079be24
Rohan Dvivedi <rohandvivedi@gmail.com>
2021-03-03 12:11:02 +0530

    added test case to insert into a tuple

--- test/test.c
+++ test/test.c
@@ -4,11 +4,23 @@
 
 #include<tuple.h>
 
-#define PAGE_SIZE    4096
-#define PAGE_LAYOUT  TUPLE_ARRAY /*SLOTTED_PAGE*/
+#define PAGE_SIZE    1024
+//#define PAGE_LAYOUT  TUPLE_ARRAY /*SLOTTED_PAGE*/
 
 // output print string
-char string[1024];
+char print_buffer[1024];
+
+typedef struct row row;
+struct row
+{
+	u8 c0;
+	i1 c1;
+	u1 c2;
+	char* c3;
+	char* c4;
+	char* c5;
+	f8 c6;
+};
 
 int main()
 {
@@ -21,17 +33,34 @@ int main()
 	insert_element_def(def,   UINT, 8);
 	insert_element_def(def,    INT, 1);
 	insert_element_def(def,   UINT, 1);
-	insert_element_def(def, STRING, VARIABLE_SIZED);
+	insert_element_def(def, STRING, VARIABLE_SIZED /*24*/);
 	insert_element_def(def, STRING, 6);
-	insert_element_def(def, STRING, 6);
+	insert_element_def(def, STRING, 10);
 	insert_element_def(def,  FLOAT, 8);
 	
 	finalize_tuple_def(def);
 
 	print_tuple_def(def);
+	printf("\n\n");
 
 	// this is your single lonely datapage
-	/*void* page = alloca(PAGE_SIZE);
+	void* page = alloca(PAGE_SIZE);
+
+	// first tuple in the page
+	void* tupl = page + 0;
+
+	row row_0 = {3003, -123, 21, "rohan is a good boy", "roopa", "DVIVEDI", 99.99};
+
+	copy_to_tuple(def, 0, tupl, &(row_0.c0));
+	copy_to_tuple(def, 1, tupl, &(row_0.c1));
+	copy_to_tuple(def, 2, tupl, &(row_0.c2));
+	copy_to_tuple(def, 3, tupl, (row_0.c3));
+	copy_to_tuple(def, 4, tupl, (row_0.c4));
+	copy_to_tuple(def, 5, tupl, (row_0.c5));
+	copy_to_tuple(def, 6, tupl, &(row_0.c6));
+
+	sprint_tuple(print_buffer, tupl, def);
+	printf("tuple : \n\t%s\n\n", print_buffer);
 
 	
 	for(int i = 0; i < PAGE_SIZE; i++)
@@ -40,8 +69,7 @@ int main()
 			printf("\n");
 		printf("[%2d](%2x)%c \t ", i, 0xff & (*((char*)(page + i))), (*((char*)(page + i))));
 	}
-	printf("\n");
-	*/
+	printf("\n\n");
 
 	return 0;
 }
