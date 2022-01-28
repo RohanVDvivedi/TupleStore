61c4e0fb3a174c8a9cc0e3a09c3e8aa9c85bc8cb
rohan <rohandvivedi@gmail.com>
2022-01-27 20:55:31 -0700

    implemented page header and utility functions

---
+++ inc/page_header.h
@@ -0,0 +1,14 @@
+#ifndef PAGE_HEADER_H
+#define PAGE_HEADER_H
+
+// returns size of page header as stored on the page
+uint32_t get_page_header_size(void* page, uint32_t page_size);
+
+// returns pointer to the page header on the page
+void* get_page_header(void* page, uint32_t page_size);
+
+// returns pointer to the end of page header on the page
+// i.e. pointer to the start of data section on the page
+void* get_page_header_end(void* page, uint32_t page_size);
+
+#endif
---
+++ src/page_header.c
@@ -0,0 +1,19 @@
+#include<page_header.h>
+
+#include<page_layout_util.h>
+
+uint32_t get_page_header_size(void* page, uint32_t page_size)
+{
+	return read_value_from_page(page, page_size);
+}
+
+void* get_page_header(void* page, uint32_t page_size)
+{
+	// page header starts after its size on the page
+	return page + get_value_size_on_page(page_size);
+}
+
+void* get_page_header_end(void* page, uint32_t page_size)
+{
+	return get_page_header(page, page_size) + get_page_header_size(page, page_size);
+}
