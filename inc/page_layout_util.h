a0b32c17cc0009ac441d04563d9915bd739b8710
rohan <rohandvivedi@gmail.com>
2022-01-27 20:55:31 -0700

    implemented page header and utility functions

---
+++ inc/page_layout_util.h
@@ -0,0 +1,8 @@
+#ifndef PAGE_LAYOUT_UTIL_H
+#define PAGE_LAYOUT_UTIL_H
+
+uint32_t get_value_size_on_page(uint32_t page_size);
+
+uint32_t read_value_from_page(void* value, uint32_t page_size);
+
+#endif
---
+++ src/page_layout_util.c
@@ -0,0 +1,24 @@
+#include<page_layout_util.h>
+
+uint32_t get_value_size_on_page(uint32_t page_size)
+{
+	if(page_size <= (1 << 8))
+		return 1;
+	else if(page_size <= (1 << 16))
+		return 2;
+	else
+		return 4;
+}
+
+uint32_t read_value_from_page(void* value, uint32_t page_size)
+{
+	switch(get_value_size_on_page(page_size))
+	{
+		case 1 :
+			return *((uint8_t*)value);
+		case 2 :
+			return *((uint16_t*)value);
+		case 4 :
+			return *((uint32_t*)value);
+	}
+}
