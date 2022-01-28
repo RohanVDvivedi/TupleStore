ddf234883f7da4d3be0ffdb49d7db8d4eb9a96f3
rohan <rohandvivedi@gmail.com>
2022-01-28 11:28:43 -0700

    page_header functions implemented

--- inc/page_layout_util.h
+++ inc/page_layout_util.h
@@ -7,4 +7,6 @@ uint32_t get_value_size_on_page(uint32_t page_size);
 
 uint32_t read_value_from_page(void* value, uint32_t page_size);
 
+void write_value_to_page(void* value, uint32_t page_size, uint32_t to_write);
+
 #endif
--- src/page_header.c
+++ src/page_header.c
@@ -4,7 +4,7 @@
 
 uint32_t get_page_header_size(void* page, uint32_t page_size)
 {
-	return read_value_from_page(page, page_size);
+	return read_value_from_page(page + 0, page_size);
 }
 
 void* get_page_header(void* page, uint32_t page_size)
@@ -13,7 +13,20 @@ void* get_page_header(void* page, uint32_t page_size)
 	return page + get_value_size_on_page(page_size);
 }
 
+int init_page_header(void* page, uint32_t page_size, uint32_t page_header_size)
+{
+	if(page_size < get_value_size_on_page(page_size) + page_header_size)
+		return 0;
+	write_value_to_page(page + 0, page_size, page_header_size);
+	return 1;
+}
+
+uint32_t get_offset_to_end_of_page_header(void* page, uint32_t page_size)
+{
+	return get_value_size_on_page(page_size) + get_page_header_size(page, page_size);
+}
+
 void* get_end_of_page_header(void* page, uint32_t page_size)
 {
-	return get_page_header(page, page_size) + get_page_header_size(page, page_size);
+	return page + get_offset_to_end_of_page_header(page, page_size);
 }
