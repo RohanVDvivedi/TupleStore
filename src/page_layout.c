1419c55ac9d0a6aa5a9e87e88fe14223e3c3ba01
rohan <rohandvivedi@gmail.com>
2022-01-28 15:47:59 -0700

    added getfreespace function to page_layout.c

--- src/page_layout.c
+++ src/page_layout.c
@@ -76,6 +76,18 @@ int swap_tuples(void* page, uint32_t page_size, const tuple_def* tpl_d, uint16_t
 
 const void* get_nth_tuple(const void* page, uint32_t page_size, const tuple_def* tpl_d, uint16_t index);
 
+uint32_t get_free_space(const void* page, uint32_t page_size, const tuple_def* tpl_d)
+{
+	switch(get_page_layout_type(tpl_d))
+	{
+		case SLOTTED_PAGE_LAYOUT :
+			return get_free_space_slotted_page(page, page_size);
+		case FIXED_ARRAY_PAGE_LAYOUT :
+			return get_free_space_fixed_array_page(page, page_size, tpl_d);
+	}
+	return 0;
+}
+
 void print_page(const void* page, uint32_t page_size, const tuple_def* tpl_d);
 
 void print_page_in_hex(const void* page, uint32_t page_size)
