77e7c0b4265ebcefee831c1dfd502b64f3ddff0a
rohan <rohandvivedi@gmail.com>
2022-02-05 16:22:41 -0700

    implemented tuple def according to the new requirements

--- inc/var_sized.h
+++ inc/var_sized.h
@@ -4,27 +4,45 @@
 #include<stdint.h>
 
 typedef struct var_string8 var_string8;
-typedef struct var_string8 var_blob8;
 struct var_string8
 {
 	uint8_t size;
-	char[] string;
+	char string[];
 };
 
 typedef struct var_string16 var_string16;
-typedef struct var_string16 var_blob16;
 struct var_string16
 {
 	uint16_t size;
-	char[] string;
+	char string[];
 };
 
 typedef struct var_string32 var_string32;
-typedef struct var_string32 var_blob32;
 struct var_string32
 {
 	uint32_t size;
-	char[] string;
+	char string[];
+};
+
+typedef struct var_blob8 var_blob8;
+struct var_blob8
+{
+	uint8_t size;
+	char blob[];
+};
+
+typedef struct var_blob16 var_blob16;
+struct var_blob16
+{
+	uint16_t size;
+	char blob[];
+};
+
+typedef struct var_blob32 var_blob32;
+struct var_blob32
+{
+	uint32_t size;
+	char blob[];
 };
 
 #endif
