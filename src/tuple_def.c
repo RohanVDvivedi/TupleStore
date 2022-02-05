b9c04d28b71c06bb3ce6846bead456dd1805a08c
rohan <rohandvivedi@gmail.com>
2022-02-05 16:48:50 -0700

    minor changes

--- src/tuple_def.c
+++ src/tuple_def.c
@@ -27,20 +27,20 @@ static int is_size_allowed_for_fixed_sized_type(element_type ele_type, uint32_t 
 		case FLOAT :
 			return (size == 4) || (size == 8);
 		case STRING :
-		case BLOB :	// STRING and BLOB can be of any size VARIABLE_SIZED
+		case BLOB :	// STRING and BLOB can be of any size
 			return 1;
 		default :
 			return 0;
 	}
 }
 
-static int is_prefix_size_allowed_for_variable_sized_type(element_type ele_type, uint32_t size)
+static int is_prefix_size_allowed_for_variable_sized_type(element_type ele_type, uint32_t prefix_size)
 {
 	switch(ele_type)
 	{
 		case VAR_STRING :
-		case VAR_BLOB :	// STRING and BLOB can be of any size VARIABLE_SIZED
-			return (size == 1) || (size == 2) || (size == 4);
+		case VAR_BLOB :
+			return (prefix_size == 1) || (prefix_size == 2) || (prefix_size == 4);
 		default :
 			return 0;
 	}
