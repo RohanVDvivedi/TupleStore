d4d43c71cd1970d0657c7b25ca52a3f7a66db4c9
rohan <rohandvivedi@gmail.com>
2022-03-23 22:19:26 -0700

    removed preposition from function definitions of fixed array page and slotted page

--- inc/page_layout_fixed_array.h
+++ inc/page_layout_fixed_array.h
@@ -29,9 +29,9 @@ void run_page_compaction_fixed_array_page(void* page, uint32_t page_size, const 
 
 uint32_t get_free_space_fixed_array_page(const void* page, uint32_t page_size, const tuple_def* tpl_d);
 
-uint32_t get_space_occupied_by_tuples_on_fixed_array_page(const void* page, uint32_t page_size, const tuple_def* tpl_d, uint32_t start_index, uint32_t last_index);
+uint32_t get_space_occupied_by_tuples_fixed_array_page(const void* page, uint32_t page_size, const tuple_def* tpl_d, uint32_t start_index, uint32_t last_index);
 
-uint32_t get_space_allotted_to_all_tuples_in_fixed_array_page(const void* page, uint32_t page_size, const tuple_def* tpl_d);
+uint32_t get_space_allotted_to_all_tuples_fixed_array_page(const void* page, uint32_t page_size, const tuple_def* tpl_d);
 
 void print_fixed_array_page(const void* page, uint32_t page_size, const tuple_def* tpl_d);
 
