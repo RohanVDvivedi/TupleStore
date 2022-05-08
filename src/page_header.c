23cdbd0c36f7f2e014a3f1129ed4f3759a8b91ae
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-08 09:28:30 -0700

    memseting the page header to all 0 bytes upon initialization

--- src/page_header.c
+++ src/page_header.c
@@ -2,6 +2,8 @@
 
 #include<page_layout_util.h>
 
+#include<string.h>
+
 uint32_t get_page_header_size(const void* page, uint32_t page_size)
 {
 	return read_value_from_page(page + 0, page_size);
@@ -17,7 +19,13 @@ int init_page_header(void* page, uint32_t page_size, uint32_t page_header_size)
 {
 	if(page_size < get_value_size_on_page(page_size) + page_header_size)
 		return 0;
+
+	// write page_header size to page
 	write_value_to_page(page + 0, page_size, page_header_size);
+
+	// memset the header to all 0 bytes
+	memset(page + get_value_size_on_page(page_size), 0, page_header_size);
+
 	return 1;
 }
 
