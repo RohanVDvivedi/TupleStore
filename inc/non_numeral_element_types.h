a908a0f0ecbc8f21b3e2bd055004a0267a5578a2
RohanVDvivedi <rohandvivedi@gmail.com>
2022-06-27 16:06:56 -0700

    implemented is_string_type and is_blob_type functions

--- inc/non_numeral_element_types.h
+++ inc/non_numeral_element_types.h
@@ -3,4 +3,8 @@
 
 #include<tuple_def.h>
 
+int is_string_type_element_def(const element_def* ele_d);
+
+int is_blob_type_element_def(const element_def* ele_d);
+
 #endif
--- src/non_numeral_element_types.c
+++ src/non_numeral_element_types.c
@@ -1,1 +1,11 @@
 #include<non_numeral_element_types.h>
+
+int is_string_type_element_def(const element_def* ele_d)
+{
+	return (ele_d->type == STRING) || (ele_d->type == VAR_STRING);
+}
+
+int is_blob_type_element_def(const element_def* ele_d)
+{
+	return (ele_d->type == BLOB) || (ele_d->type == BLOB);
+}
