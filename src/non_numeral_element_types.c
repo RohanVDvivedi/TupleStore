6c450b04d4657172b3d0981f4ea6895dea0d5474
RohanVDvivedi <rohandvivedi@gmail.com>
2022-06-27 16:07:59 -0700

    minor error

--- src/non_numeral_element_types.c
+++ src/non_numeral_element_types.c
@@ -7,5 +7,5 @@ int is_string_type_element_def(const element_def* ele_d)
 
 int is_blob_type_element_def(const element_def* ele_d)
 {
-	return (ele_d->type == BLOB) || (ele_d->type == BLOB);
+	return (ele_d->type == BLOB) || (ele_d->type == VAR_BLOB);
 }
