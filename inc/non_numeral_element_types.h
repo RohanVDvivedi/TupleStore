4f18e3c7b55495af7a683ade2079215b152bf952
RohanVDvivedi <rohandvivedi@gmail.com>
2022-06-27 16:21:43 -0700

    implemented element_size and data_size functions

--- inc/non_numeral_element_types.h
+++ inc/non_numeral_element_types.h
@@ -7,4 +7,16 @@ int is_string_type_element_def(const element_def* ele_d);
 
 int is_blob_type_element_def(const element_def* ele_d);
 
+// for variable sized blob or string i.e. VAR_BLOB or VAR_STRING,
+// the first element_def.size_specifier_prefix_size number of bytes are used to store the size of the actual data
+// and then the actual data begins
+// VAR_STRING are not NULL terminated, unlike the STRING which may be NULL terminated, if the string length is lesser than its fixed size
+
+// this only includes the actual size of the data
+// does not include the bytes required to store the size of this data (i.e. does not include element_def.size_specifier_prefix_size)
+uint32_t get_data_size_for_variable_sized_non_numeral_element(const void* e, const element_def* ele_d);
+
+// this includes the complete size of the data
+uint32_t get_element_size_for_variable_sized_non_numeral_element(const void* e, const element_def* ele_d);
+
 #endif
--- src/non_numeral_element_types.c
+++ src/non_numeral_element_types.c
@@ -1,5 +1,7 @@
 #include<non_numeral_element_types.h>
 
+#include<int_accesses.h>
+
 int is_string_type_element_def(const element_def* ele_d)
 {
 	return (ele_d->type == STRING) || (ele_d->type == VAR_STRING);
@@ -8,4 +10,14 @@ int is_string_type_element_def(const element_def* ele_d)
 int is_blob_type_element_def(const element_def* ele_d)
 {
 	return (ele_d->type == BLOB) || (ele_d->type == VAR_BLOB);
+}
+
+uint32_t get_data_size_for_variable_sized_non_numeral_element(const void* e, const element_def* ele_d)
+{
+	return read_uint32(e, ele_d->size_specifier_prefix_size);
+}
+
+uint32_t get_element_size_for_variable_sized_non_numeral_element(const void* e, const element_def* ele_d)
+{
+	return ele_d->size_specifier_prefix_size + get_data_size_for_variable_sized_non_numeral_element(e, ele_d);
 }
