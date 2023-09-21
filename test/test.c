49bd4a66b252b2cb3d520e55a75b2b9a0ce48d28
RohanVDvivedi <rohandvivedi@gmail.com>
2023-09-20 23:08:51 -0700

    refactored test cases to just work with new api

--- test/test.c
+++ test/test.c
@@ -138,38 +138,38 @@ int main()
 
 	r = &(row){-123, 5, "Rohan is good boy", 99.99};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = append_tuple(page, PAGE_SIZE, def, tuple_cache);
+	res = append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache);
 	printf("Insert : %d\n\n\n", res);
 
 	// ---------------	INSERT
 
 	r = &(row){-12, 12, "Rohan is bad", 51.2};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	append_tuple(page, PAGE_SIZE, def, tuple_cache);
+	append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache);
 	printf("Insert : %d\n\n\n", res);
 
 	// ---------------`INSERT
 
 	r = &(row){-12, 12, "Rohan is awesome person", 520.21};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	append_tuple(page, PAGE_SIZE, def, tuple_cache);
+	append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache);
 	printf("Insert : %d\n\n\n", res);
 
 	// ---------------	INSERT
 
 	r = &(row){-53, 17, "Rohan", 20.21};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	append_tuple(page, PAGE_SIZE, def, tuple_cache);
+	append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache);
 	printf("Insert : %d\n\n\n", res);
 
 	// ---------------	COMPARE 2 TUPLES
 
 	printf("compare(tuple_2 , tuple_3) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 2), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 2), 
 								def,
 								((uint32_t[]){2}),
-								get_nth_tuple(page, PAGE_SIZE, def, 3), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 3), 
 								def,
 								((uint32_t[]){2}),
 								1)
@@ -177,10 +177,10 @@ int main()
 
 	printf("compare(tuple_3 , tuple_2) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 3), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 3), 
 								def,
 								((uint32_t[]){2}),
-								get_nth_tuple(page, PAGE_SIZE, def, 2), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 2), 
 								def,
 								((uint32_t[]){2}),
 								1)
@@ -188,10 +188,10 @@ int main()
 
 	printf("compare(tuple_0 , tuple_1) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 0), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 0), 
 								def,
 								NULL,
-								get_nth_tuple(page, PAGE_SIZE, def, 1), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 1), 
 								def,
 								NULL,
 								def->element_count)
@@ -199,10 +199,10 @@ int main()
 
 	printf("compare(tuple_0 , tuple_1) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 0), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 0), 
 								def,
 								((uint32_t[]){2}),
-								get_nth_tuple(page, PAGE_SIZE, def, 1), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 1), 
 								def,
 								((uint32_t[]){2}),
 								1)
@@ -210,10 +210,10 @@ int main()
 
 	printf("compare(tuple_1 , tuple_2) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 1), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 1), 
 								def,
 								NULL,
-								get_nth_tuple(page, PAGE_SIZE, def, 2), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 2), 
 								def,
 								NULL,
 								def->element_count)
@@ -221,10 +221,10 @@ int main()
 
 	printf("compare(tuple_1 , tuple_2) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 1), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 1), 
 								def,
 								((uint32_t[]){2, 3}),
-								get_nth_tuple(page, PAGE_SIZE, def, 2), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 2), 
 								def,
 								((uint32_t[]){2, 3}),
 								2)
@@ -232,10 +232,10 @@ int main()
 
 	printf("compare(tuple_1 , tuple_2) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 1), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 1), 
 								def,
 								((uint32_t[]){1, 0}),
-								get_nth_tuple(page, PAGE_SIZE, def, 2), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 2), 
 								def,
 								((uint32_t[]){1, 0}),
 								2)
@@ -243,10 +243,10 @@ int main()
 
 	printf("compare(tuple_1 , tuple_2) = %d\n\n", 
 				compare_tuples(
-								get_nth_tuple(page, PAGE_SIZE, def, 1), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 1), 
 								def,
 								((uint32_t[]){3, 2}),
-								get_nth_tuple(page, PAGE_SIZE, def, 2), 
+								get_nth_tuple_on_page(page, PAGE_SIZE, def, 2), 
 								def,
 								((uint32_t[]){3, 2}),
 								2)
@@ -265,7 +265,7 @@ int main()
 
 	r = &(row){-456, 18, "project by Rohan", 65536};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 1, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 1, tuple_cache);
 	printf("Update(1) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -281,7 +281,7 @@ int main()
 
 	r = &(row){-789, 9, "Name: Rohan Dvivedi", 99.99};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 3, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 3, tuple_cache);
 	printf("Update(3) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -295,7 +295,7 @@ int main()
 
 	// --------------- DELETE
 
-	res = delete_tuple(page, PAGE_SIZE, def, 1);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 1, NULL);
 	printf("Delete(1) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -309,7 +309,7 @@ int main()
 
 	// --------------- DELETE
 
-	res = delete_tuple(page, PAGE_SIZE, def, 3);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 3, NULL);
 	printf("Delete(3) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -325,7 +325,7 @@ int main()
 
 	r = &(row){-234, 11, "Project by Rohan.", 655.36};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 1, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 1, tuple_cache);
 	printf("Update(1) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -357,8 +357,8 @@ int main()
 
 	r = &(row){34, 173, "likes genre horror.", 7.121996};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	printf("Can insert : %d\n\n", can_append_tuple(page, PAGE_SIZE, def, tuple_cache));
-	printf("Insert : %d\n\n\n\n", append_tuple(page, PAGE_SIZE, def, tuple_cache));
+	printf("Can insert : %d\n\n", can_append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
+	printf("Insert : %d\n\n\n\n", append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
 
 	// ---------------- PRINT PAGE
 
@@ -369,8 +369,8 @@ int main()
 
 	r = &(row){-35, 13, "Rohan is an artist.", 7.12};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	printf("Can insert : %d\n\n", can_append_tuple(page, PAGE_SIZE, def, tuple_cache));
-	printf("Insert : %d\n\n\n\n", append_tuple(page, PAGE_SIZE, def, tuple_cache));
+	printf("Can insert : %d\n\n", can_append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
+	printf("Insert : %d\n\n\n\n", append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
 
 	// ---------------- PRINT PAGE
 
@@ -381,8 +381,8 @@ int main()
 
 	r = &(row){36, 153, "Rohan is THE Rohan Dvivedi", 12.1996};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	printf("Can insert : %d\n\n", can_append_tuple(page, PAGE_SIZE, def, tuple_cache));
-	printf("Insert : %d\n\n\n\n", append_tuple(page, PAGE_SIZE, def, tuple_cache));
+	printf("Can insert : %d\n\n", can_append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
+	printf("Insert : %d\n\n\n\n", append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
 
 	// ---------------- PRINT PAGE
 
@@ -393,8 +393,8 @@ int main()
 
 	r = &(row){36, 143, "Rohan is just Rohan", 12.1996};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	printf("Can insert : %d\n\n", can_append_tuple(page, PAGE_SIZE, def, tuple_cache));
-	printf("Insert : %d\n\n\n\n", append_tuple(page, PAGE_SIZE, def, tuple_cache));
+	printf("Can insert : %d\n\n", can_append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
+	printf("Insert : %d\n\n\n\n", append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
 
 	// ---------------- PRINT PAGE
 
@@ -405,8 +405,8 @@ int main()
 
 	r = &(row){35, 49, "Rohan is Dvivedi", 7.1996};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	printf("Can insert : %d\n\n", can_append_tuple(page, PAGE_SIZE, def, tuple_cache));
-	printf("Insert : %d\n\n\n\n", append_tuple(page, PAGE_SIZE, def, tuple_cache));
+	printf("Can insert : %d\n\n", can_append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
+	printf("Insert : %d\n\n\n\n", append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache));
 
 	// ---------------- PRINT PAGE
 
@@ -421,7 +421,7 @@ int main()
 
 	r = &(row){-237, 35, "Rohan is THE Rohan Dvivedi", 655.36};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 5, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 5, tuple_cache);
 	printf("Update(5) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -433,7 +433,7 @@ int main()
 
 	r = &(row){-234, 5, "Project TupleStore", 655.36};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 5, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 5, tuple_cache);
 	printf("Update(5) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -445,7 +445,7 @@ int main()
 
 	r = &(row){-234, 3, "Project TupleStore", 655.36};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 5, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 5, tuple_cache);
 	printf("Update(5) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -457,7 +457,7 @@ int main()
 
 	r = &(row){1, 7, "Rohan is THE Rohan Dvivedi of this project", 7.12};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 5, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 5, tuple_cache);
 	printf("Update(5) : %d\n\n\n", res);
 
 	// ---------------	PRINT PAGE
@@ -477,54 +477,54 @@ int main()
 	// ---------------  COMPACT PAGE
 
 	memmove(temp_page, page, PAGE_SIZE);
-	delete_tuple(temp_page, PAGE_SIZE, def, 0);
-	delete_tuple(temp_page, PAGE_SIZE, def, 2);
+	update_tuple_on_page(temp_page, PAGE_SIZE, def, 0, NULL);
+	update_tuple_on_page(temp_page, PAGE_SIZE, def, 2, NULL);
 
-	printf("\nBefore compaction available_size(%u)\n", get_free_space(temp_page, PAGE_SIZE, def));
+	printf("\nBefore compaction available_size(%u)\n", get_free_space_on_page(temp_page, PAGE_SIZE, def));
 	printf("\nTEMP PAGE :: \n");
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	run_page_compaction(temp_page, PAGE_SIZE, def, 1, 1);
+	run_page_compaction(temp_page, PAGE_SIZE, def);
 
-	printf("\nAfter compaction available_size(%u)\n", get_free_space(temp_page, PAGE_SIZE, def));
+	printf("\nAfter compaction available_size(%u)\n", get_free_space_on_page(temp_page, PAGE_SIZE, def));
 	printf("\nTEMP PAGE :: \n");
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
 	// ---------------  SWAP TUPLES
 
-	printf("\nSwap(%u, %u) = %d\n", 2, 3, swap_tuples(temp_page, PAGE_SIZE, def, 2, 3));
+	printf("\nSwap(%u, %u) = %d\n", 2, 3, swap_tuples_on_page(temp_page, PAGE_SIZE, def, 2, 3));
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	printf("\nSwap(%u, %u) = %d\n", 0, 3, swap_tuples(temp_page, PAGE_SIZE, def, 0, 3));
+	printf("\nSwap(%u, %u) = %d\n", 0, 3, swap_tuples_on_page(temp_page, PAGE_SIZE, def, 0, 3));
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	printf("\nSwap(%u, %u) = %d\n", 4, 3, swap_tuples(temp_page, PAGE_SIZE, def, 4, 3));
+	printf("\nSwap(%u, %u) = %d\n", 4, 3, swap_tuples_on_page(temp_page, PAGE_SIZE, def, 4, 3));
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	printf("\nSwap(%u, %u) = %d\n", 1, 3, swap_tuples(temp_page, PAGE_SIZE, def, 1, 3));
+	printf("\nSwap(%u, %u) = %d\n", 1, 3, swap_tuples_on_page(temp_page, PAGE_SIZE, def, 1, 3));
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	delete_tuple(temp_page, PAGE_SIZE, def, 0);
+	update_tuple_on_page(temp_page, PAGE_SIZE, def, 0, NULL);
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	printf("\nSwap(%u, %u) = %d\n", 0, 5, swap_tuples(temp_page, PAGE_SIZE, def, 0, 5));
+	printf("\nSwap(%u, %u) = %d\n", 0, 5, swap_tuples_on_page(temp_page, PAGE_SIZE, def, 0, 5));
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	printf("\nSwap(%u, %u) = %d\n", 0, 3, swap_tuples(temp_page, PAGE_SIZE, def, 0, 3));
+	printf("\nSwap(%u, %u) = %d\n", 0, 3, swap_tuples_on_page(temp_page, PAGE_SIZE, def, 0, 3));
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
 	// ---------------  DELETE ALL TUPLES AT ONCE
 
-	delete_all_tuples(temp_page, PAGE_SIZE, def);
+	discard_all_tuples_on_page(temp_page, PAGE_SIZE, def);
 	print_page(temp_page, PAGE_SIZE, def);
 	printf("\n\n");
 
@@ -534,16 +534,16 @@ int main()
 	print_page(page, PAGE_SIZE, def);
 	printf("\n\n");
 
-	uint16_t tuples_to_delete = get_tuple_count(page, PAGE_SIZE, def);
+	uint16_t tuples_to_delete = get_tuple_count_on_page(page, PAGE_SIZE, def);
 	uint16_t mid = (tuples_to_delete/2);
 
 	for(uint16_t i = mid; i < tuples_to_delete; i++)
 	{
 		uint16_t index = i;
 		printf("l1 index : %u\n", index);
-		if(exists_tuple(page, PAGE_SIZE, def, index))
+		if(exists_tuple_on_page(page, PAGE_SIZE, def, index))
 		{
-			int is_deleted = delete_tuple(page, PAGE_SIZE, def, index);
+			int is_deleted = update_tuple_on_page(page, PAGE_SIZE, def, index, NULL);
 			printf("Delete called for index %u, giving : %d\n", index, is_deleted);
 			print_page(page, PAGE_SIZE, def);
 			printf("\n\n");
@@ -554,9 +554,9 @@ int main()
 	{
 		uint16_t index = mid - 1 - i;
 		printf("l2 index : %u\n", index);
-		if(exists_tuple(page, PAGE_SIZE, def, index))
+		if(exists_tuple_on_page(page, PAGE_SIZE, def, index))
 		{
-			int is_deleted = delete_tuple(page, PAGE_SIZE, def, index);
+			int is_deleted = update_tuple_on_page(page, PAGE_SIZE, def, index, NULL);
 			printf("Delete called for index %u, giving : %d\n", index, is_deleted);
 			print_page(page, PAGE_SIZE, def);
 			printf("\n\n");
@@ -567,13 +567,13 @@ int main()
 
 	r = &(row){3, 76, "Rohan Dvivedi's, project.", 3.57};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 2, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 2, tuple_cache);
 	printf("Update(2) : %d\n\n\n", res);
 	print_page(page, PAGE_SIZE, def);
 
 	r = &(row){5, 94, "Rohan's, project : Simple Tuple Storage Model.", 35.7};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = update_tuple(page, PAGE_SIZE, def, 0, tuple_cache);
+	res = update_tuple_on_page(page, PAGE_SIZE, def, 0, tuple_cache);
 	printf("Update(0) : %d\n\n\n", res);
 	print_page(page, PAGE_SIZE, def);
 
@@ -581,13 +581,13 @@ int main()
 
 	r = &(row){3, 76, "Rohan Dvivedi's, project.", 3.57};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = append_tuple(page, PAGE_SIZE, def, tuple_cache);
+	res = append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache);
 	printf("insert : %d\n\n\n", res);
 	print_page(page, PAGE_SIZE, def);
 
 	r = &(row){5, 94, "Rohan's, project : Simple Tuple Storage Model.", 35.7};
 	build_tuple_from_row_struct(def, tuple_cache, r);
-	res = append_tuple(page, PAGE_SIZE, def, tuple_cache);
+	res = append_tuple_on_page(page, PAGE_SIZE, def, tuple_cache);
 	printf("insert : %d\n\n\n", res);
 	print_page(page, PAGE_SIZE, def);
 
