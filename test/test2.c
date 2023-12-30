7863ab37f50bd70e0c4cd07a8f9102493ac11486
RohanVDvivedi <rohandvivedi@gmail.com>
2023-12-29 18:40:07 -0700

    added logic to build desired tuple def with varying element types

--- test/test2.c
+++ test/test2.c
@@ -6,7 +6,80 @@
 #include<page_layout.h>
 
 #define MAX_TUPLE_SIZE 4096
-char tuple[MAX_TUPLE_SIZE];
+char my_tuple[MAX_TUPLE_SIZE];
+
+tuple_def* get_tuple_definition()
+{
+	// initialize tuple definition and insert element definitions
+	tuple_def* def = get_new_tuple_def("my_table", 16, PAGE_SIZE);
+
+	int res = insert_element_def(def, "col_0", BIT_FIELD, 5, 0, NULL);
+	if(res == 0)
+	{
+		printf("failed adding column 0\n");
+		exit(-1);
+	}
+
+	res = insert_element_def(def, "col_1", LARGE_UINT, 5, 0, NULL);
+	if(res == 0)
+	{
+		printf("failed adding column 1\n");
+		exit(-1);
+	}
+
+	res = insert_element_def(def, "var_col_2", VAR_STRING, 2, 0, NULL);
+	if(res == 0)
+	{
+		printf("failed adding column 2\n");
+		exit(-1);
+	}
+
+	res = insert_element_def(def, "col_3", INT, 1, 0, NULL);
+	if(res == 0)
+	{
+		printf("failed adding column 3\n");
+		exit(-1);
+	}
+
+	res = insert_element_def(def, "col_4", BIT_FIELD, 9, 0, NULL);
+	if(res == 0)
+	{
+		printf("failed adding column 4\n");
+		exit(-1);
+	}
+
+	res = insert_element_def(def, "var_col_5", VAR_STRING, 2, 0, NULL);
+	if(res == 0)
+	{
+		printf("failed adding column 5\n");
+		exit(-1);
+	}
+
+	res = insert_element_def(def, "col_6", INT, 1, 0, NULL);
+	if(res == 0)
+	{
+		printf("failed adding column 6\n");
+		exit(-1);
+	}
+
+	res = finalize_tuple_def(def);
+	if(res == 0)
+	{
+		printf("failed finalizing tuple definition\n");
+		exit(-1);
+	}
+
+	if(is_empty_tuple_def(def))
+	{
+		printf("ERROR BUILDING TUPLE DEFINITION\n");
+		exit(-1);
+	}
+
+	print_tuple_def(def);
+	printf("\n\n");
+
+	return def;
+}
 
 int main()
 {
