e82b32437f5e623156ade865a4aacae1bec0ed1c
RohanVDvivedi <rohandvivedi@gmail.com>
2023-12-29 18:26:07 -0700

    added test 2 to test large_uint and bit_field

---
+++ test/test2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include<alloca.h>
+
+#include<tuple.h>
+#include<page_layout.h>
+
+int main()
+{
+	return 0;
+}
--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,1 +1,2 @@
 gcc -Wall ./test.c -o test.out -ltuplestore -lserint -lcutlery
+gcc -Wall ./test2.c -o test2.out -ltuplestore -lserint -lcutlery
