b7569bd941bce50a7eafd65335900c2f6fec83f4
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-17 22:38:42 +0530

    completed implementation of get size function

--- src/data_info.c
+++ src/data_info.c
@@ -28,9 +28,16 @@ uint32_t get_size_for_type_info(const data_type_info* dti, const void* data)
 	uint32_t element_count = read_value_from_page(data + get_offset_to_prefix_element_count_for_container_type_info(dti), get_bytes_required_for_prefix_element_count_for_container_type_info(dti));
 
 	// now we know for sure that this is variable sized container, but without size in its prefix
-	// so this must be a container precisely : variable sized string, variable sized blob or array of variable element count but of fixed length arrays
+	// so this must be a container precisely : variable sized string, variable sized blob or array of variable element count but of fixed length type
 	// all in all we know the element_count and that each element is fixed sized element
-	// TODO
+	
+	if(dti->containee == BIT_FIELD)
+		return get_value_size_on_page(dti->max_size)
+		 + bitmap_size_in_bytes(element_count * (needs_is_valid_bit_in_prefix_bitmap(dti->containee) + dti->containee->bit_field_size));
+	else
+		return get_value_size_on_page(dti->max_size)
+		 + bitmap_size_in_bytes(element_count * needs_is_valid_bit_in_prefix_bitmap(dti->containee))
+		 + element_count * dti->containee.size;
 }
 
 int is_container_type_info(const data_type_info* dti)
