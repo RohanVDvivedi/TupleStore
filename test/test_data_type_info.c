2fd200db0deec35cdd37fcf91d6b895643b3d5f2
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-22 22:46:23 +0530

    --

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -5,31 +5,31 @@
 int main()
 {
 	finalize_type_info(BIT_FIELD_33_NULLABLE);
-	print_type_info(BIT_FIELD_3_NULLABLE);
+	print_type_info(BIT_FIELD_33_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(BIT_FIELD_33_NON_NULLABLE);
-	print_type_info(BIT_FIELD_3_NON_NULLABLE);
+	print_type_info(BIT_FIELD_33_NON_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(UINT_5_NULLABLE);
-	print_type_info(UINT_3_NULLABLE);
+	print_type_info(UINT_5_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(UINT_5_NON_NULLABLE);
-	print_type_info(UINT_3_NON_NULLABLE);
+	print_type_info(UINT_5_NON_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(INT_5_NULLABLE);
-	print_type_info(INT_3_NULLABLE);
+	print_type_info(INT_5_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(INT_5_NON_NULLABLE);
-	print_type_info(INT_3_NON_NULLABLE);
+	print_type_info(INT_5_NON_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(FLOAT_double_NULLABLE);
-	print_type_info(FLOAT_long_double_NULLABLE);
+	print_type_info(FLOAT_double_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(FLOAT_long_double_NON_NULLABLE);
@@ -37,11 +37,11 @@ int main()
 	printf("\n");
 
 	finalize_type_info(LARGE_UINT_21_NULLABLE);
-	print_type_info(LARGE_UINT_3_NULLABLE);
+	print_type_info(LARGE_UINT_21_NULLABLE);
 	printf("\n");
 
 	finalize_type_info(LARGE_UINT_21_NON_NULLABLE);
-	print_type_info(LARGE_UINT_3_NON_NULLABLE);
+	print_type_info(LARGE_UINT_21_NON_NULLABLE);
 	printf("\n");
 	return 0;
 }
