e7e056d30d4656d5f0949e23cb5617295b600310
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-22 22:44:18 +0530

    printing all primitive types

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -1,8 +1,47 @@
 #include<data_type_info.h>
 
+#include<stdio.h>
+
 int main()
 {
-	finalize_type_info(UINT_3_NULLABLE);
+	finalize_type_info(BIT_FIELD_33_NULLABLE);
+	print_type_info(BIT_FIELD_3_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(BIT_FIELD_33_NON_NULLABLE);
+	print_type_info(BIT_FIELD_3_NON_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(UINT_5_NULLABLE);
 	print_type_info(UINT_3_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(UINT_5_NON_NULLABLE);
+	print_type_info(UINT_3_NON_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(INT_5_NULLABLE);
+	print_type_info(INT_3_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(INT_5_NON_NULLABLE);
+	print_type_info(INT_3_NON_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(FLOAT_double_NULLABLE);
+	print_type_info(FLOAT_long_double_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(FLOAT_long_double_NON_NULLABLE);
+	print_type_info(FLOAT_long_double_NON_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(LARGE_UINT_21_NULLABLE);
+	print_type_info(LARGE_UINT_3_NULLABLE);
+	printf("\n");
+
+	finalize_type_info(LARGE_UINT_21_NON_NULLABLE);
+	print_type_info(LARGE_UINT_3_NON_NULLABLE);
+	printf("\n");
 	return 0;
 }
