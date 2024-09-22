8a38b444ff0f77c70a1ea53d37a815ec2774847e
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-22 22:26:20 +0530

    --

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	finalize_data_type_info(UINT_3_NULLABLE);
-	print_data_type_info(UINT_3_NULLABLE);
+	finalize_type_info(UINT_3_NULLABLE);
+	print_type_info(UINT_3_NULLABLE);
 	return 0;
 }
