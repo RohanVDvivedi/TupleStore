96eb81b181784fafc11ca1c5dd82cf99c0a56614
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-26 22:33:31 +0530

    testing with double

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -10,7 +10,7 @@ int main()
 	initialize_tuple_data_type_info(tuple_type_info, "tuple_type1", 1, 4096, 4);
 	tuple_type_info->containees[0].type_info = UINT_3_NON_NULLABLE;
 	tuple_type_info->containees[1].type_info = BIT_FIELD_5_NON_NULLABLE;
-	tuple_type_info->containees[2].type_info = INT_3_NULLABLE;
+	tuple_type_info->containees[2].type_info = FLOAT_double_NULLABLE;
 	tuple_type_info->containees[3].type_info = BIT_FIELD_5_NULLABLE;
 	finalize_type_info(tuple_type_info);
 	print_type_info(tuple_type_info);printf("\n");
@@ -22,14 +22,14 @@ int main()
 
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (user_value){.uint_value = 5});
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (user_value){.bit_field_value = 0x6});
-	set_user_value_to_containee_in_container(tuple_type_info, tuple, 2, 0, (user_value){.int_value = -5});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 2, 0, (user_value){.double_value = -5});
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 3, 0, (user_value){.bit_field_value = 0x36});
 
 	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
 
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (user_value){.uint_value = 99});
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (user_value){.bit_field_value = 0x25});
-	set_user_value_to_containee_in_container(tuple_type_info, tuple, 2, 0, (user_value){.int_value = 55});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 2, 0, (user_value){.double_value = 55});
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 3, 0, (user_value){.bit_field_value = 0x25});
 
 	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
