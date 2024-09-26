adad3987572994e7fe30fd626955cee8efeb3142
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-26 22:28:53 +0530

    making and testing with tuple type info of 4 elements

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -6,10 +6,12 @@
 
 int main()
 {
-	data_type_info* tuple_type_info = alloca(sizeof_tuple_data_type_info(2));
-	initialize_tuple_data_type_info(tuple_type_info, "tuple_type1", 1, 4096, 2);
+	data_type_info* tuple_type_info = alloca(sizeof_tuple_data_type_info(4));
+	initialize_tuple_data_type_info(tuple_type_info, "tuple_type1", 1, 4096, 4);
 	tuple_type_info->containees[0].type_info = UINT_3_NON_NULLABLE;
 	tuple_type_info->containees[1].type_info = BIT_FIELD_5_NON_NULLABLE;
+	tuple_type_info->containees[2].type_info = INT_3_NULLABLE;
+	tuple_type_info->containees[3].type_info = BIT_FIELD_5_NULLABLE;
 	finalize_type_info(tuple_type_info);
 	print_type_info(tuple_type_info);printf("\n");
 
