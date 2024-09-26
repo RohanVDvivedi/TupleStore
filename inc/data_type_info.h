005e4b56be28fa73749da2919104860744499b8d
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-26 16:53:25 +0530

    minor changes to comments and added function declarations

--- inc/data_type_info.h
+++ inc/data_type_info.h
@@ -237,12 +237,19 @@ uint32_t initialize_minimal_data_for_type_info(const data_type_info* dti, void* 
 int set_containee_to_NULL_in_container(const data_type_info* dti, void* data, uint32_t index);
 
 // data here is assumed to contain garbage
-// returns 1 if the below function will succeed
+// max_size_increment_allowed is never a problem for fixed length elements
 int can_set_user_value_for_type_info(const data_type_info* dti, void* data, uint32_t max_size_increment_allowed, user_value uval);
 
 // returns 0, if the max_size_increment is violated OR the dti is BIT_FIELD type OR uval is NULL
-// max_size_increment is never a proble from fixed length elements
-// it is essentially a copy operation
+// max_size_increment_allowed is never a problem for fixed length elements
 int set_user_value_for_type_info(const data_type_info* dti, void* data, uint32_t max_size_increment_allowed, user_value uval);
+
+// returns 0, if the max_size_increment is violated OR uval is NULL and the element is non-NULLABLE
+// max_size_increment_allowed is never a problem for fixed length elements
+int can_set_user_value_to_containee_in_container(const data_type_info* dti, void* data, uint32_t index, uint32_t max_size_increment_allowed, user_value uval);
+
+// returns 0, if the max_size_increment is violated OR uval is NULL and the element is non-NULLABLE
+// max_size_increment_allowed is never a problem for fixed length elements
+int set_user_value_to_containee_in_container(const data_type_info* dti, void* data, uint32_t index, uint32_t max_size_increment_allowed, user_value uval);
 
 #endif
