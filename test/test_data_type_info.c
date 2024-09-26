86f43f37eef5ff64c894ede7d8304fa41d4f25ec
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-26 22:32:16 +0530

    testing NULLABLE types for the same types

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -22,16 +22,22 @@ int main()
 
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (user_value){.uint_value = 5});
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (user_value){.bit_field_value = 0x6});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 2, 0, (user_value){.int_value = -5});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 3, 0, (user_value){.bit_field_value = 0x36});
 
 	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
 
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (user_value){.uint_value = 99});
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (user_value){.bit_field_value = 0x25});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 2, 0, (user_value){.int_value = 55});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 3, 0, (user_value){.bit_field_value = 0x25});
 
 	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
 
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (*NULL_USER_VALUE));
 	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (*NULL_USER_VALUE));
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 2, 0, (*NULL_USER_VALUE));
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 3, 0, (*NULL_USER_VALUE));
 
 	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
 
