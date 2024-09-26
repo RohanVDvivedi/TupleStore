0cec1456ed7315dcb751c447063c6b654cdd4e14
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-26 22:26:04 +0530

    tested setting fixed sized elements

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -9,12 +9,27 @@ int main()
 	data_type_info* tuple_type_info = alloca(sizeof_tuple_data_type_info(2));
 	initialize_tuple_data_type_info(tuple_type_info, "tuple_type1", 1, 4096, 2);
 	tuple_type_info->containees[0].type_info = UINT_3_NON_NULLABLE;
-	tuple_type_info->containees[1].type_info = BIT_FIELD_3_NON_NULLABLE;
+	tuple_type_info->containees[1].type_info = BIT_FIELD_5_NON_NULLABLE;
 	finalize_type_info(tuple_type_info);
 	print_type_info(tuple_type_info);printf("\n");
 
 	char tuple[4096];
 	initialize_minimal_data_for_type_info(tuple_type_info, tuple);
+
+	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
+
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (user_value){.uint_value = 5});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (user_value){.bit_field_value = 0x6});
+
+	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
+
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (user_value){.uint_value = 99});
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (user_value){.bit_field_value = 0x25});
+
+	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
+
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 0, 0, (*NULL_USER_VALUE));
+	set_user_value_to_containee_in_container(tuple_type_info, tuple, 1, 0, (*NULL_USER_VALUE));
 
 	print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
 
