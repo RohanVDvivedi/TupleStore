6a3f69e61dcb81dcdd33dbe874971c1c045317a4
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-28 16:45:20 +0530

    recreated a bug of not able to reach a power of 2 max size

--- test/test_data_type_info.c
+++ test/test_data_type_info.c
@@ -13,7 +13,7 @@ int main()
 		data_type_info s4 = get_variable_length_string_type(300);
 
 		data_type_info* tuple_type_info = alloca(sizeof_tuple_data_type_info(8));
-		initialize_tuple_data_type_info(tuple_type_info, "tuple_type1", 1, 4096, 8);
+		initialize_tuple_data_type_info(tuple_type_info, "tuple_type1", 1, 256, 8);
 		tuple_type_info->containees[0].type_info = UINT_3_NON_NULLABLE;
 		tuple_type_info->containees[1].type_info = BIT_FIELD_5_NON_NULLABLE;
 		tuple_type_info->containees[2].type_info = &s3;
@@ -25,7 +25,7 @@ int main()
 		finalize_type_info(tuple_type_info);
 		print_type_info(tuple_type_info);printf("\n");
 
-		char tuple[4096];
+		char tuple[256];
 		initialize_minimal_data_for_type_info(tuple_type_info, tuple);
 
 		print_data_for_data_type_info(tuple_type_info, tuple);printf("\n");
@@ -52,7 +52,7 @@ int main()
 
 		set_user_value_to_containee_in_container(tuple_type_info, tuple, 3, 0, (user_value){.string_value = "GHIJK", .string_size = strlen("GHIJK")});
 		set_user_value_to_containee_in_container(tuple_type_info, tuple, 4, 0, (user_value){.string_value = "Rohan Dvivedi", .string_size = strlen("Rohan Dvivedi")});
-		set_user_value_to_containee_in_container(tuple_type_info, tuple, 5, 300, (user_value){.string_value = "Vipulkumar Dvivedi", .string_size = strlen("Vipulkumar Dvivedi")});
+		set_user_value_to_containee_in_container(tuple_type_info, tuple, 5, 300, (user_value){.string_value = "Vipulkumar DvivediXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4444444444444444444444444444444444411111111111111111111111111111111111111111111111111111111", .string_size = strlen("Vipulkumar DvivediXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4444444444444444444444444444444444411111111111111111111111111111111111111111111111111111111")});
 		set_user_value_to_containee_in_container(tuple_type_info, tuple, 6, 0, (user_value){.double_value = 55});
 		set_user_value_to_containee_in_container(tuple_type_info, tuple, 7, 0, (user_value){.bit_field_value = 0x25});
 
