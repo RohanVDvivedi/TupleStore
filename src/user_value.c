ad6935aabdcbeb94d16fb60f23bc98b13590e22a
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-01 09:37:53 +0530

    empty user value is same as zero user value

--- src/user_value.c
+++ src/user_value.c
@@ -15,7 +15,7 @@ user_value const * const DEFAULT_USER_VALUE = &((const user_value){});
 user_value const * const NULL_USER_VALUE = &((const user_value){.is_NULL = 1, .is_OUT_OF_BOUNDS = 0,});
 user_value const * const OUT_OF_BOUNDS_USER_VALUE = &((const user_value){.is_NULL = 1, .is_OUT_OF_BOUNDS = 1,});
 user_value const * const ZERO_USER_VALUE = &((const user_value){.is_NULL = 0, .is_OUT_OF_BOUNDS = 0,});
-user_value const * const EMPTY_USER_VALUE = &((const user_value){.is_NULL = 0, .is_OUT_OF_BOUNDS = 0, .data = NULL, .data_size = 0});
+user_value const * const EMPTY_USER_VALUE = ZERO_USER_VALUE;
 
 int is_user_value_NULL(const user_value* uval)
 {
