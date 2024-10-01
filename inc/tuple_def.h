dd90597176ff7a7c0eecbac87ec06d6927b04c36
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-01 19:27:21 +0530

    declared tuple_def and tupe_size_def

--- inc/tuple_def.h
+++ inc/tuple_def.h
@@ -7,27 +7,30 @@
 typedef struct tuple_size_def tuple_size_def;
 struct tuple_size_def
 {
-	// set to 1 for a variable sized tuple_def
-	// The tuple is variable sized, if atleast one of the element_defs is variable sized
 	int is_variable_sized;
 
-	// size of offsets (to variable sized elements) and tuple_size (stored as the prefix of the tuple for variable sized tuples) stored in the variable length tuples
-	uint32_t size_of_byte_offsets;
-
-	// min total size of tuple in bytes
-	// i.e. size of tuple when all variable sized elements are NULL
-	// this will be same as tuple's size for a fixed sized tuple
 	union
 	{
-		// defined for variable sized tuple
-		uint32_t min_size;
-
-		// defined for fixed sized tuple
-		uint32_t size;
+		uint32_t size; // -> to be used directly if the is_variable_sized = 0
+		uint32_t min_size // -> suggests minimum size of the tuple for a variable sized tuple
 	};
 
-	// max_size of the tuple
-	uint32_t max_size;
+	// below two fields are used only when is_variable_sized = 1
+	int has_size_in_prefix; // -> if set directly read size from the prefix and we are done
+	int has_element_count_in_pefix; // -> need to do more work, to know the size if has_size_inprefix = 0, but has_element_count_in_prefix = 1
+
+	uint32_t max_size; // to read size OR element_count from prefix using read_value_from_page(, max_size)
+
+	// below attributes are only required when is_variable_sized = 1, has_size_in_prefix = 0, and has_element_count_in_pefix = 1
+	// this also implies that the element_count is variable, but the individual element is fixed size (may or may not be nullable)
+	// this is the case with variable size string, variable sized blob, and variable element count arrays of fixed sized elements
+	int does_containee_need_is_valid_bit_in_prefix;
+	int is_containee_bit_field; // -> can either be a bitfield or not
+	union
+	{
+		uint32_t containee_size;
+		uint32_t containee_bit_field_size;
+	}; // -> if is_containee_bit_field gives containee_bit_field_size in bits, else the actual size of fixed sized containee in the containee_size attribute
 };
 
 typedef struct tuple_def tuple_def;
