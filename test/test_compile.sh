6ad0fbbcfc61fd923e7815def0a148067179c866
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-03 21:23:53 +0530

    yet to make the old test cases compilable they need hash and compare functions

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,3 @@
-gcc -Wall ./test.c -o test.out -ltuplestore -lserint -lcutlery
-gcc -Wall ./test2.c -o test2.out -ltuplestore -lserint -lcutlery
+#gcc -Wall ./test.c -o test.out -ltuplestore -lserint -lcutlery
+#gcc -Wall ./test2.c -o test2.out -ltuplestore -lserint -lcutlery
 gcc -Wall ./test_data_type_info.c -o test_data_type_info.out -ltuplestore -lserint -lcutlery
