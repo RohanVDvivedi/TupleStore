7ef7794692b96d478f890adfb75ec2586f85bc7b
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-13 13:57:18 +0530

    minor function signature change

--- src/tuple.c
+++ src/tuple.c
@@ -576,7 +576,7 @@ int compare_tuples2(const void* tup1, const void* tup2, const tuple_def* tpl_d, 
 	return compare;
 }
 
-int compare_user_values3(const user_value* uvals1, const user_value* uvals2, const data_type_info** dtis, const compare_direction* cmp_dir, uint32_t element_count)
+int compare_user_values3(const user_value* uvals1, const user_value* uvals2, data_type_info const * const * dtis, const compare_direction* cmp_dir, uint32_t element_count)
 {
 	int compare = 0;
 	for(uint32_t i = 0; ((i < element_count) && (compare == 0)); i++)
