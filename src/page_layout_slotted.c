2561322a74184c6d6bec8006971d805f4f4ccba4
RohanVDvivedi <rohandvivedi@gmail.com>
2026-08-09 18:57:41 +0530

    minor fix

--- src/page_layout_slotted.c
+++ src/page_layout_slotted.c
@@ -277,7 +277,7 @@ int insert_tuple_slotted_page(void* page, uint32_t page_size, const tuple_size_d
 		// read the last tuple's offset which is the new tuple's offset
 		uint32_t new_tuple_s_offset_val = get_offset_to_ith_tuple(page, page_size, tuple_count_val - 1);
 
-		for(uint32_t to_index = index + 1; to_index < tuple_count_val; to_index++)
+		for(uint32_t to_index = (tuple_count_val - 1); to_index > index; to_index--)
 		{
 			uint32_t from_index = to_index - 1;
 			write_value_to_page(page + get_offset_to_ith_tuple_offset(page, page_size, to_index), page_size, get_offset_to_ith_tuple(page, page_size, from_index));
